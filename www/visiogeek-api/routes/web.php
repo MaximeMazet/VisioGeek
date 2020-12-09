4fddc0603345775e59245700c1ad4964a0ddd098
Maxime Mazet <contact@maximemazet.fr>
2020-12-09 22:54:17 +0100

    delete and create route

--- www/visiogeek-api/routes/web.php
+++ www/visiogeek-api/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
